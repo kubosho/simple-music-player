832930112a5b448d7fd501c51d13b2f2fd2ed81e
kubosho <ta2@o2p.jp>
2014-10-24 14:36:53 +0900

    Add tests

--- scripts/test/test.js
+++ scripts/test/test.js
@@ -1,13 +1,39 @@
-describe('test', function () {
-    beforeEach(function () {
-        this.browserMusicPlayer = new BrowserMusicPlayer();
+describe('Music player in browser', function () {
+    this.browserMusicPlayer = new BrowserMusicPlayer();
+
+    describe('#loadMusic()', function () {
+        it('should load music file', function () {
+            assert(0 === 1);
+        });
     });
 
-    it('play sound', function () {
-        assert();
+    describe('#isMusicFile()', function () {
+        it('should is music file', function () {
+            assert(0 === 1);
+        });
     });
 
-    it('stop sound', function () {
-        assert();
+    describe('#play()', function () {
+        it('should play sound', function () {
+            assert(0 === 1);
+        });
+    });
+
+    describe('#stop()', function () {
+        it('should stop sound', function () {
+            assert(0 === 1);
+        });
+    });
+
+    describe('#rewind()', function () {
+        it('should rewind sound', function () {
+            assert(0 === 1);
+        });
+    });
+
+    describe('#forward()', function () {
+        it('should forward sound', function () {
+            assert(0 === 1);
+        });
     });
 });
