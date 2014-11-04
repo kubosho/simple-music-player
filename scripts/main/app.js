8fffb508ce9af51a824448a15f2530bdd5aca6bd
kubosho <ta2@o2p.jp>
2014-11-04 10:22:10 +0900

    Update

--- scripts/main/app.js
+++ scripts/main/app.js
@@ -4,14 +4,17 @@
 
     document.addEventListener('DOMContentLoaded', function () {
         var musicPlayer = new MusicPlayer();
+        var hasSource = false;
+        var isPlay = false;
 
         new Vue({
             el: '#app',
             data: {
+                names: [],
                 sources: []
             },
             methods: {
-                loadMusic: function(event) {
+                loadMusic: function (event) {
                     var that = this;
                     var files = event.target.files;
 
@@ -19,7 +22,32 @@
                         musicPlayer.loadMusic(file, function (src) {
                             return that.$data.sources.push(src);
                         });
+
+                        if (!musicPlayer.isMusicFile) {
+                            return;
+                        }
+                        that.$data.names.push(file.name);
                     });
+                },
+                toggle: function (event) {
+                    var elm = event.currentTarget;
+                    var index = this.$data.names.indexOf(elm.textContent);
+                    var audioElms = document.getElementsByTagName('audio'),
+                        audioE = audioElms[index];
+
+                    if (!hasSource) {
+                        musicPlayer.createSource(audioE);
+                        hasSource = true;
+                    }
+
+                    if (!isPlay) {
+                        musicPlayer.play(audioE);
+                        isPlay = true;
+                        return;
+                    }
+
+                    musicPlayer.stop(audioE);
+                    isPlay = false;
                 }
             }
         });
