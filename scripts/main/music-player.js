82ffa5de88c435b42c53d968d558cee2f4545022
kubosho <ta2@o2p.jp>
2014-11-05 20:37:48 +0900

    Fix not identify a ogg format

--- scripts/main/music-player.js
+++ scripts/main/music-player.js
@@ -38,19 +38,19 @@
         var fileType = file.type;
         var type = fileType.substring(6, 9);
 
-        if (/(mp3|wav)/.test(type)) {
+        if (/(mp3|wav|ogg)/.test(type)) {
             return true;
         }
 
         // ref: Mime types - http://help.dottoro.com/lapuadlp.php
-        // type are (m4a || ogg || wma) ?
+        // type are (m4a || wma) ?
         if (/x-./.test(type)) {
             type = fileType.substring(6, 11);
             // type is wma ?
             type = type !== 'x-ms-' ? type : fileType.substring(6, 14);
         }
 
-        if (/(^x-(m4a|ogg|ms-wma))/.test(type)) {
+        if (/(^x-(m4a|ms-wma))/.test(type)) {
             return true;
         }
 
