94e435a5ae639cb75c22600b1048b9b44a32ea74
kubosho <ta2@o2p.jp>
2014-11-06 23:07:06 +0900

    Change argument (File: File object -> File.type: string)

--- scripts/main/music-player.js
+++ scripts/main/music-player.js
@@ -24,7 +24,7 @@
 
         reader.readAsDataURL(file);
         reader.onload = function () {
-            if (!that.isMusicFile(file)) {
+            if (!that.isMusicFile(file.type)) {
                 return console.warn('Warning: Not a music file');
             }
 
@@ -34,9 +34,8 @@
         return reader;
     };
 
-    MusicPlayer.prototype.isMusicFile = function (file) {
-        var fileType = file.type;
-        var type = fileType.substring(6, 9);
+    MusicPlayer.prototype.isMusicFile = function (filetype) {
+        var type = filetype.substring(6, 9);
 
         if (/(mp3|wav|ogg)/.test(type)) {
             return true;
@@ -45,9 +44,9 @@
         // ref: Mime types - http://help.dottoro.com/lapuadlp.php
         // type are (m4a || wma) ?
         if (/x-./.test(type)) {
-            type = fileType.substring(6, 11);
+            type = filetype.substring(6, 11);
             // type is wma ?
-            type = type !== 'x-ms-' ? type : fileType.substring(6, 14);
+            type = type !== 'x-ms-' ? type : filetype.substring(6, 14);
         }
 
         if (/(^x-(m4a|ms-wma))/.test(type)) {
