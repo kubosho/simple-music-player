d340845981981789a14f7151755383c860a1c61e
kubosho <ta2@o2p.jp>
2014-11-07 09:10:27 +0900

    Add gulp jade task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var tslint = require('gulp-tslint');
+var jade = require('gulp-jade');
+
+gulp.task('jade', function() {
+    gulp.src('./src/views/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('./dist/'))
+});
 
 gulp.task('typescript', function() {
     gulp.src('src/scripts/**/*.ts')
+        .pipe(tslint())
+        .pipe(tslint.report('verbose'))
         .pipe(ts({
             declarationFiles: true,
             target: 'es5'
-        })
-        .pipe(tslint())
-        .pipe(tslint.report('verbose')));
+        }));
 });
 
 gulp.task('default', function () {
